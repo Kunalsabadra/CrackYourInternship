2557f5bf6f7223e1495caa347587ed67e374f77f
Kunal Sabadra <85892457+Kunalsabadra@users.noreply.github.com>
2022-05-13 08:56:37 +0530

    Update 11.duplicateNumb.cpp

--- Arrays/11.duplicateNumb.cpp
+++ Arrays/11.duplicateNumb.cpp
@@ -1,20 +1,49 @@
-#include<bits/stdc++.h>
-
-using namespace std;
-int findDuplicate(vector < int > & arr) {
-  int n = arr.size();
-  int freq[n + 1] = {0};
-  for (int i = 0; i < n; i++) {
-    if (freq[arr[i]] == 0) {
-      freq[arr[i]] += 1;
-    } else {
-      return arr[i];
+//  Code Using Set T.C:O(n) , S.C : O(n)
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n = nums.size();
+      
+//       Vector to store duplicate number
+       vector<int> ans;
+        
+//       Set to store unique numbers
+        set<int> s;
+        for(auto num:nums){
+            
+          //If already present then it is a duplicate number
+            if(s.find(num) != s.end())
+                ans.push_back(num);
+            
+//           if not present already in set then add it to set 
+            else  s.insert(num);
+        }
+        
+        return ans;
     }
-  }
-  return 0;
-}
-int main() {
-  vector < int > arr;
-  arr = {1,3,4,2,3};
-  cout << "The duplicate element is " << findDuplicate(arr) << endl;
-}
+};
+
+
+// Optimised using constant space
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        int n = nums.size();
+       vector<int> ans;
+        
+       for(int i=0; i<n; i++){           
+         
+         //Get the index , the element corresponds to
+           int index =  abs(nums[i])-1;
+         
+//If the no is already negative, it means we are encountering it twice          
+           if(nums[index] <0)
+               ans.push_back(index+1);
+           
+// Flip the number to the index to negative         
+           nums[index] = nums[index]*-1;
+       }
+        return ans;
+    }
+};
