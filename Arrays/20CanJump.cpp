140262b92c06659723c44aef251a839cb41e2012
Kunal Sabadra <85892457+Kunalsabadra@users.noreply.github.com>
2022-06-07 10:25:41 +0530

    Create 20CanJump.cpp

---
+++ Arrays/20CanJump.cpp
@@ -0,0 +1,14 @@
+//Greedy Method
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int reachable =0;
+        for(int i=0;i<n; i++){
+            if(reachable < i) return false;
+            reachable = max(reachable , i+nums[i]);
+        }
+        // cout<<reachable<<" "<<endl;
+        return true;
+    }
+};
