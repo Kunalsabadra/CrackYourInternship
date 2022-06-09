33a8a40dc83e20b2befee63477dc6bfdab763489
Kunal Sabadra <85892457+Kunalsabadra@users.noreply.github.com>
2022-06-09 10:44:23 +0530

    Create 24Permutations.cpp

---
+++ Arrays/24Permutations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+     void Permutation(vector<int> &ds , vector<int> &nums , vector<vector<int>> &ans, int freq[]){
+         int n = nums.size();
+         if(ds.size() == n){
+             ans.push_back(ds);
+             return;
+         }
+         for(int i=0; i<n; i++){
+             if(!freq[i]){
+                 ds.push_back(nums[i]);
+                 freq[i] = 1;
+                 Permutation(ds , nums , ans,freq);
+                 freq[i] =0;
+                 ds.pop_back();
+             }
+         }
+     }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        int freq[nums.size()];
+        for(int i=0; i<nums.size(); i++) freq[i] =0;
+            Permutation(ds,nums,ans,freq);
+            return ans;
+        
+    }
+};
+
